5023e2dee21dcf013bfea4c8a120f3d0feff2548
Nasimjon Yusupov <nasimjon078@gmail.com>
2021-06-15 12:41:10 +0500

    push first commit in laravel/yajra

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Student;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class StudentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Student::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'username' =>  $this->faker->userName(),
+            'phone' => $this->faker->phoneNumber(),
+            'dob' =>  $this->faker->date($format = 'Y-m-d', $max = 'now'),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function unverified()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
+    }
+}
